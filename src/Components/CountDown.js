dc8e350d45f3fec669886b969285cd7a75ace73e
RomanLetodiani <136558219+RomaLetodiani@users.noreply.github.com>
2023-08-22 13:12:41 +0400

    Update CountDown.js

--- src/Components/CountDown.js
+++ src/Components/CountDown.js
@@ -4,7 +4,7 @@ const CountDown = (props) => {
   return (
     <div className="flex flex-col justify-between items-center rounded-lg w-28 p-4 bg-[#000000b3] ">
       <p className={`text-4xl ${props.numbers.length < 3 ? 'w-[2ch]' : ''} `}>
-        {props.numbers < 9 ? `0${props.numbers}` : props.numbers}
+        {props.numbers <= 9 ? `0${props.numbers}` : props.numbers}
       </p>
       <p>{props.name}</p>
     </div>
